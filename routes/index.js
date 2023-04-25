34ee7e7412a6f634f9ecc094d92c7da7a87732a4
Peter-98 <pma751@inlumine.ual.es>
2023-04-25 12:30:11 +0200

    build

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,9 @@
-const express = require('express');
-const router = express.Router();
-const ctrlLocations = require('../controllers/locations');
-const ctrlReviews = require('../controllers/reviews');
+var express = require('express');
+var router = express.Router();
 
-// Locations 
-router.get('/locations/', ctrlLocations.locationsReadAll);
-router.post('/locations', ctrlLocations.locationsCreate);
-
-router.get('/locations/:locationid', ctrlLocations.locationsReadOne);
-router.put('/locations/:locationid', ctrlLocations.locationsUpdateOne);
-router.delete('/locations/:locationid', ctrlLocations.locationsDeleteOne);
-
-// Reviews
-router.post('/locations/:locationid/reviews', ctrlReviews.reviewsCreate);
-
-router.get('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsReadOne);
-router.put('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsUpdateOne);
-router.delete('/locations/:locationid/reviews/:reviewid', ctrlReviews.reviewsDeleteOne);
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
