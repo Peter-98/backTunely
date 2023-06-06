1c8d18eb853a0239a4642d527a36687b7a0926c0
Peter-98 <pma751@inlumine.ual.es>
2023-06-06 10:41:47 +0200

    clean

--- controllers/tracksController.js
+++ controllers/tracksController.js
@@ -26,7 +26,6 @@ async function getToken() {
     }
   });
   const accessToken = data.access_token;
-  console.log('Token obtenido: ', accessToken);
   process.env.ACCESS_TOKEN = accessToken;
 
   // Llamar al método getToken nuevamente después de 1 hora (3600 segundos * 1000 milisegundos)
